29e2117d06760ebe0f3a015cad29e2bf818a5a6d
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2022-06-04 14:44:33 +0600

    fixed page linking

--- hadabiyat/apps/maangas/urls.py
+++ hadabiyat/apps/maangas/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:maanga_id>/', views.detail, name = 'detail'),
     path('<int:maanga_id>/leave_comment', views.leave_comment, name = 'leave_comment'),
-    path('<int:maanga_id>/<int:chapter_id>', views.detail, name = 'detail'),
+    path('<int:maanga_id>/<int:chapter_id>', views.chapter, name = 'chapter'),
 ]
 
--- hadabiyat/apps/maangas/views.py
+++ hadabiyat/apps/maangas/views.py
@@ -10,37 +10,45 @@ def index(request):
     search_query = request.GET.get('q')
 
     if search_query == None:
-        latest_maanga_list = Maanga.objects.order_by('-maanga_publ')[:5]
+        maanga_list = Maanga.objects.order_by('-maanga_publ')
     else:
-        latest_maanga_list = Maanga.objects.filter(maanga_title__icontains = search_query).order_by('-maanga_publ')
+        maanga_list = Maanga.objects.filter(maanga_title__icontains = search_query).order_by('-maanga_publ')
     
-    return render(request, 'maangas/list.html', {'latest_maanga_list':latest_maanga_list})
+    return render(request, 'maangas/list.html', {'maanga_list':maanga_list})
 
 
-def detail(request, maanga_id, chapter_id):
+def detail(request, maanga_id):
     try:
         a = Maanga.objects.get(id = maanga_id)
-        ch = Chapter.objects.get(id = chapter_id)
     except:
         return render(request, 'maangas/error.html')
 
     latest_comments_list = a.comment_set.order_by('-id')[:10]
-    chapters_list = a.chapter_set.order_by('id')
+    chapters_list = list(a.chapter_set.order_by('id'))
 
     return render(request, 'maangas/detail.html', {'maanga': a,
-    'chapter_id': ch.id, 
-    'latest_comments_list': latest_comments_list, 
+    'latest_comments_list': latest_comments_list,
     'chapters_list': chapters_list})
 
 
 def chapter(request, maanga_id, chapter_id):
     try:
         a = Maanga.objects.get(id = maanga_id)
-        ch = Chapter.objects.get(id = chapter_id)
     except:
         return render(request, 'maangas/error.html')
+    
+    chapters_list = list(a.chapter_set.order_by('id'))
+    all_chapters = list(Chapter.objects.all())
 
-    return render(request, 'maangas/chapter.html', {'chapter': ch, 'maanga_title': a.maanga_title})
+    chapter_num = all_chapters.index(chapters_list[chapter_id-1])+1
+    
+    ch = Chapter.objects.get(id = chapter_num)
+
+    return render(request, 'maangas/chapter.html', {'chapter': ch, 'maanga_title': a.maanga_title,
+    'chapter_id':chapter_id,
+    'chapters_list':chapters_list,
+    'chapter_num': chapter_num,
+    'all_chapters':all_chapters})
 
 
 def leave_comment(request, maanga_id):
